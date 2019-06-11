fc85ffe6cea12b596a4112cb2507e3df1516d002
Seungmin4035 <51221044+Seungmin4035@users.noreply.github.com>
2019-06-11 11:24:22 +0900

    Create sub.h

---
+++ sub.h
@@ -0,0 +1,10 @@
+#ifndef SUB_H
+#define SUB_H
+
+void printWarning(char *message);
+void printLayout();
+void putValue(char *pValue, int size, int x, int y);
+int isInteger(char *str, int len);
+void gotoxy(int x, int y);
+
+#endif
